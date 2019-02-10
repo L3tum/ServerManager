56db2633ad8f7b188b667acacf9c38119fa46cc0
L3tum <9307432+L3tum@users.noreply.github.com>
2019-02-10 13:01:27 +0100

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -1,16 +1,9 @@
 $ErrorActionPreference = 'Stop';
 Write-Host Starting build
 
-If ($isWindows) {
-	docker build --pull -t riase:linux-arm -f Dockerfile.arm32v7 .
-	docker build --pull -t riase:linux-amd64 -f Dockerfile.alpine .
-	docker build --pull -t riase:windows-amd64 -f Dockerfile.1809 .
-	docker build --pull -t riase:windows-arm -f Dockerfile.1809-arm32v7 .
-} Else{
-	docker build -t riase:linux-arm -f Dockerfile.arm32v7 .
-	docker build -t riase:linux-amd64 -f Dockerfile.alpine .
-	docker build -t riase:windows-amd64 -f Dockerfile.1809 .
-	docker build -t riase:windows-arm -f Dockerfile.1809-arm32v7 .
-}
+docker build -t riase:linux-arm -f Dockerfile.arm32v7 .
+docker build -t riase:linux-amd64 -f Dockerfile.alpine .
+docker build -t riase:windows-amd64 -f Dockerfile.1809 .
+docker build -t riase:windows-arm -f Dockerfile.1809-arm32v7 .
 
 docker images
